e3ab7758fe0334af2914cce927808b0db4d2c290
PRIYANSH GOYAL <priyanshgoyal3904@gmail.com>
2024-09-28 19:47:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import LocalEventsPage from "./components/Features/LocalEventsPage";
+import HiddenGemsPage from "./components/Features/HiddenGemsPage";
+import LocalGuide from "./components/Features/LocalGuide";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import FeaturesSection from "./components/FeaturesSection";
+import AboutSection from "./components/AboutSection";
+import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
+  
+import "./App.css";
+
+function App() {
+  return (
+    
+    <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <HeroSection />
+              <FeaturesSection />
+              <AboutSection />
+              <Testimonials />
+              <Footer />
+            </>
+          } />
+          <Route path="/local-events" element={<LocalEventsPage />} />
+          <Route path="/hidden-gems" element={<HiddenGemsPage />} />
+          <Route path="/local-guide" element={<LocalGuide/>} />
+        </Routes>
+      </div>
+    
+  );
+}
+
+export default App;
